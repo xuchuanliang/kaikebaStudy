ced6cbfa2c5c67e644b8f57d75025a69479b4e95
xuchuanliang361@163.com <Xu1993082700>
2019-11-08 08:05:17 +0800

    spring boot

--- _05spring_boot/src/main/java/com/hkbt/Application.java
+++ _05spring_boot/src/main/java/com/hkbt/Application.java
@@ -1,10 +1,16 @@
-package com.hkbt._05spring_boot;
+package com.hkbt;
 
+import com.hkbt.dto.UserDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 public class Application {
+
+    @Resource
+    private UserDto userDto;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ _05spring_boot/src/main/java/com/hkbt/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.hkbt.dto;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@PropertySource("classpath:my.properties")
+@ConfigurationProperties("user")
+public class UserDto {
+    private String name;
+    private String gender;
+    private int age;
+}
